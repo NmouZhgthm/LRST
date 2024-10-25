ae8c9079bee1fb224cac272144262dd0a838a9f4
nmouzhgthm <1735599305@qq.com>
2024-10-25 21:12:21 +0800

    更新 API 支持模板

--- api/index.py
+++ api/index.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, 
+    template_folder='../templates',
+    static_folder='../static'
+)
 
 @app.route('/')
 def home():
-    return "Hello from LRST!"
+    return render_template('index.html')
 
 @app.route('/api/test')
 def test():
