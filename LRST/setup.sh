d3891412d59d7afa5ddf6068252b7b019e92e880
nmouzhgthm <1735599305@qq.com>
2024-10-25 21:05:40 +0800

    清理项目结构

--- LRST/setup.sh
+++
@@ -1,7 +0,0 @@
-mkdir LRST
-cd LRST
-mkdir static templates
-touch app.py requirements.txt README.md
-mkdir static/css static/js static/images
-touch static/css/style.css
-touch templates/base.html templates/index.html
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 # Vercel 需要这个处理程序
